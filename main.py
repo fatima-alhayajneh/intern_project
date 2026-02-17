6eae4a034f2d956bb62843d4df89c269cb88d531
Fatima Al-Hayajneh <2021980250@ses.yu.edu.jo>
2026-02-17 16:10:10 +0300

    feat: implement Employee class and testing script

---
+++ main.py
@@ -0,0 +1,10 @@
+from models import Employee
+
+
+emp1 = Employee("Fatima", "Computer Engineer", 1200)
+emp2 = Employee("Marco", "Software Trainee", 1000)
+
+emp1.display_info()
+emp2.display_info()
+
+
